46a0b40870f315877973ed5d1525b25e660fa2fd
Tolulope Olorunfemi <ayodele.tolu12@gmail.com>
2021-09-03 23:49:56 +0000

    modification made on area and circum

---
+++ areaofcircle.py
@@ -0,0 +1,7 @@
+#2 The script below performs different operation on circle
+# The script will request for user to enter the radius of a circle
+
+radius_of_circle = float(input("Kindly enter the radius of the circle: "))
+area_of_circle = 3.14 * radius_of_circle**2
+print(f"The area of the circle with radius {radius_of_circle} is {area_of_circle} ")
+
